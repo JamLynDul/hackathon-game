30e4cd923024707e55f0c8aaa7f96aec0cd101e3
JamLynDul <jamielynnd24@gmail.com>
2026-02-20 14:33:54 -0600

    New player sprites, cheese collectible
    Updated player run sprite.
    Added player idle sprite.
    Added the cheese collectible.
    (does not do anything yet.)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,12 @@
 extends CharacterBody2D
 
 
-const SPEED = 250.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -22,6 +22,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction == 0:
+		animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("run")	
 		
 	if direction > 0:
 		animated_sprite_2d.flip_h = true
