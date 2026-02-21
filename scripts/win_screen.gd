e41fde772f18cccc5e835d29dfac0e1d83c09c04
JamLynDul <jamielynnd24@gmail.com>
2026-02-21 10:50:58 -0600

    Win jingle
    Added a jingle to the win screen.

--- scripts/win_screen.gd
+++ scripts/win_screen.gd
@@ -1,4 +1,13 @@
 extends Control
 
+@onready var animation_player = $AnimationPlayer
+var played = false
+
+func _process(_delta: float) -> void:
+	if played == false:
+		animation_player.play("win_jingle")
+		played = true
+	
+	
 func _on_back_to_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
