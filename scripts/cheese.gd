c84bb30dafe22590e2a93c434e96dd99438ffe96
JamLynDul <jamielynnd24@gmail.com>
2026-02-20 22:26:11 -0600

    Sound effect implementation, "cheese" script modifications
    Tweaked the "cheese" script in order to allow for a sound effect to play upon pickup via an AnimationPlayer node.

--- scripts/cheese.gd
+++ scripts/cheese.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
+@onready var animation_player = $AnimationPlayer
+
 # "Collects" the cheese.
 func _on_body_entered(_body: Node2D) -> void:
-	
-	queue_free()
+	animation_player.play("pickup")
